60ffb4020dda839fb1f71523ee4e1fd331ebc962
shopex112 <68697019+shopex112@users.noreply.github.com>
2026-01-07 14:16:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/app.jsx'
-import '@/index.css'
+import App from './App.jsx'
+import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './auth/AuthContext'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
 )
